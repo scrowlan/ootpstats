37d5146f8f621fd44cf86865045b271152d1c525
Scott Rowland <scrowlan@gmail.com>
2025-04-17 12:10:20 -0400

    fixed mismatched end

--- app/services/statistic_import_service.rb
+++ app/services/statistic_import_service.rb
@@ -1,36 +1,35 @@
 class StatisticImportService
   require 'csv'
 
-  CSV.open(file, 'r', headers: true) do |csv|
-  headers = csv.first.headers
-
-  # Detect and rename duplicate headers dynamically
-  unique_headers = {}
-  renamed_headers = headers.map do |header|
-    if unique_headers[header]
-      unique_headers[header] += 1
-      "#{header}_#{unique_headers[header]}"  # Rename as "Hits_2"
-    else
-      unique_headers[header] = 1
-      header  # Keep the first occurrence as is
-    end
-  end
-
-
   def call(file, tournament_id, tourneydate)
     opened_file = File.open(file)
-    
 
+    CSV.open(opened_file, 'r', headers: true) do |csv|
+      headers = csv.first.headers
 
-    CSV.foreach(opened_file, **options) do |row|
+      # Detect and rename duplicate headers dynamically
+      unique_headers = {}
+      renamed_headers = headers.map do |header|
+        if unique_headers[header]
+          unique_headers[header] += 1
+          "#{header}_#{unique_headers[header]}"  # Rename as "Hits_2"
+        else
+          unique_headers[header] = 1
+          header  # Keep the first occurrence as is
+        end
+      end
 
-      statistic = Statistic.new row.to_hash
-      statistic.tournament_id = tournament_id
-      statistic.tourneydate = tourneydate
+      options = { headers: renamed_headers, header_converters: :symbol }
 
-      statistic.save!
-      # for performance, you could create a separate job to import each user
-      # CsvImportJob.perform_later(tournament_hash)
+      CSV.foreach(opened_file, **options) do |row|
+        statistic = Statistic.new row.to_hash
+        statistic.tournament_id = tournament_id
+        statistic.tourneydate = tourneydate
+
+        statistic.save!
+        # for performance, you could create a separate job to import each user
+        # CsvImportJob.perform_later(tournament_hash)
+      end
     end
   end
 end
