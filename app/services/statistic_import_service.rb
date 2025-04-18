b3309db44aac2db0d1bdd963c1962a31d107c396
Scott Rowland <scrowlan@gmail.com>
2025-04-17 21:52:27 -0400

    getting upload to work again

--- app/services/statistic_import_service.rb
+++ app/services/statistic_import_service.rb
@@ -3,33 +3,19 @@ class StatisticImportService
 
   def call(file, tournament_id, tourneydate)
     opened_file = File.open(file)
+    options = { headers: ['cid', 'name', 'val', 'cyear', 'org', 'title', 'cc', 'clvl', 'hitterg', 'hittergs', 'pa', 'hitterab', 'h', 'hitter1b', 'hitter2b', 'hitter3b', 'hitterhr', 'rbi', 'hitterr', 'hitterbb', 'hitteribb', 'hitterhp', 'hittersh', 'hittersf', 'ci', 'so', 'gidp', 'hittertb', 'pi/pa', 'sb', 'cs', 'pitcherg', 'pitchergs', 'w', 'l', 'svo', 'sv', 'bs', 'hld', 'sd', 'md', 'ip', 'bf', 'pitcherab', 'ha', 'pitcher1b', 'pitcher2b', 'pitcher3b', 'pitcherhr', 'pitchertb', 'pitcherr', 'er', 'pitcherbb', 'pitcheribb', 'k', 'pitcherhp', 'era', 'pitchersh', 'pitchersf', 'qs', 'pos', 'tc', 'a', 'po', 'e', 'dp', 'pct', 'rng', 'zr', 'eff', 'tournament_id', 'tourneydate'], skip_lines: "CID", col_sep: ',' }
 
-    CSV.open(opened_file, 'r', headers: true) do |csv|
-      headers = csv.first.headers
 
-      # Detect and rename duplicate headers dynamically
-      unique_headers = {}
-      renamed_headers = headers.map do |header|
-        if unique_headers[header]
-          unique_headers[header] += 1
-          "#{header}_#{unique_headers[header]}"  # Rename as "Hits_2"
-        else
-          unique_headers[header] = 1
-          header  # Keep the first occurrence as is
-        end
-      end
 
-      options = { headers: renamed_headers, header_converters: :symbol }
+    CSV.foreach(opened_file, **options) do |row|
 
-      CSV.foreach(opened_file, **options) do |row|
-        statistic = Statistic.new row.to_hash
-        statistic.tournament_id = tournament_id
-        statistic.tourneydate = tourneydate
+      statistic = Statistic.new row.to_hash
+      statistic.tournament_id = tournament_id
+      statistic.tourneydate = tourneydate
 
-        statistic.save!
-        # for performance, you could create a separate job to import each user
-        # CsvImportJob.perform_later(tournament_hash)
-      end
+      statistic.save!
+      # for performance, you could create a separate job to import each user
+      # CsvImportJob.perform_later(tournament_hash)
     end
   end
 end
